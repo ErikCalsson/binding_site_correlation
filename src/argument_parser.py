7e7aecf64fa2a1ae5a6f5bf3a980527969da9b23
Erik Carlßon <ecarlsson@techfak.uni-bielefeld.de>
2020-10-10 17:49:49 +0200

    added argument for own degree of freedom

--- src/argument_parser.py
+++ src/argument_parser.py
@@ -3,13 +3,14 @@ import argparse  # parsing arguments from terminal
 
 # imports intern
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--bed1", help="first input file as name or path", required=True)
 parser.add_argument("--bed2", help="second input file as name or path", required=True)
 parser.add_argument("--outfile", help="output file name or path", nargs='?')
 parser.add_argument("--display", help="may be True for graphics in browser else always console output", nargs='?')
 parser.add_argument("--alpha", help="custom level of significance, default is 0.05", nargs='?')
-# TODO chi² arguments for p value and freedom degrees
+parser.add_argument("--freedom", help="for specific degree of freedom chi square test")
 
 # for accessing parsed arguments
 args = parser.parse_args()
