09f8c7e187e3ba919760726cf771a2eb62d818a5
Erik Carlßon <ecarlsson@techfak.uni-bielefeld.de>
2020-10-13 09:27:39 +0200

    cleanup comments

--- src/binding_site_correlation.py
+++ src/binding_site_correlation.py
@@ -5,8 +5,6 @@ import src.argument_parser as pars
 import src.output_browser as gui
 import src.output_terminal as pix
 
-# TODO check whether terminal or dash output is to be used, launch accordingly
-
 
 def launch_me():
     if pars.args.display:
