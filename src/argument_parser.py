5ec66f2a18790970de5b557bd304afecb9679b66
Erik Carlßon <ecarlsson@techfak.uni-bielefeld.de>
2020-12-17 21:14:31 +0100

    added fasta file and k-mer min value to argpase

--- src/argument_parser.py
+++ src/argument_parser.py
@@ -7,10 +7,12 @@ import argparse  # parsing arguments from terminal
 parser = argparse.ArgumentParser()
 parser.add_argument("--bed1", help="first input file as name or path", required=True)
 parser.add_argument("--bed2", help="second input file as name or path", required=True)
+parser.add_argument("--fasta", help="FASTA reference input file as name or path", required=True)
 parser.add_argument("--outfile", help="output file name or path", nargs='?')
 parser.add_argument("--display", help="may be True for graphics in browser else always console output", nargs='?')
 parser.add_argument("--alpha", help="custom level of significance, default is 0.05", nargs='?')
 parser.add_argument("--freedom", help="for specific degree of freedom chi square test")
+parser.add_argument("--kmin", help="define the minimum k-mer length")
 
 # for accessing parsed arguments
 args = parser.parse_args()
