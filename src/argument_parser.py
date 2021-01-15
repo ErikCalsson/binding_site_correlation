bbf6b16a4ec4cba6a289a04c694713a9757193ba
Erik Carlßon <ecarlsson@techfak.uni-bielefeld.de>
2021-01-15 11:37:27 +0100

    added kmax as max k-mer length as optional argument

--- src/argument_parser.py
+++ src/argument_parser.py
@@ -13,6 +13,7 @@ parser.add_argument("--display", help="may be True for graphics in browser else 
 parser.add_argument("--alpha", help="custom level of significance, default is 0.05", nargs='?')
 parser.add_argument("--freedom", help="for specific degree of freedom chi square test")
 parser.add_argument("--kmin", help="define the minimum k-mer length")
+parser.add_argument("--kmax", help="define the maximum k-mer length")
 
 # for accessing parsed arguments
 args = parser.parse_args()
